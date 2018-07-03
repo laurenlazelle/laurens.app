e11b6558cecc9c75c3a5f8a24288a10908c19f98
Tom LaZelle <tomlazelle@yahoo.com>
2018-07-03 17:27:40 -0500

    first commit

---
+++ laurens.app/Controllers/AnimalsController.cs
@@ -0,0 +1,20 @@
+﻿
+using laurens.app.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace laurens.app.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new AnimalModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(AnimalModel model)
+        {
+            return View(new AnimalModel());
+        }
+    }
+}
---
+++ laurens.app/Models/AnimalModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace laurens.app.Models
+{
+    public class AnimalModel
+    {
+        public string OwnersName { get; set; }
+        public string DogOrCat { get; set; }
+        public string AnimalsName { get; set; }
+    }
+}
